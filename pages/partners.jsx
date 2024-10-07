a8dc0daee6f9bc0379c4ae9fe6d32dcd60122402
pogoosstudio <dioverrdm@gmail.com>
2024-10-07 17:12:49 -0400

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -9,7 +9,7 @@ export default function Partners() {
       </div>
                 <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
                     <i className="fal fa-stars text-amber-400 mr-2" />
-                    Partners
+                    Socios
                 </h1>
             </div>
 
@@ -18,7 +18,7 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://discord.gg/6XGqdgE">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/68ef946cf7cc39e4063eed4d446f6dda.webp" />
+                            <img className="rounded-full h-32 w-32" src="/img/logo.jpg" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
                                 <div className="flex items-center mt-1">
@@ -34,7 +34,7 @@ export default function Partners() {
                     <div className="flex flex-col justify-center text-white rounded">
                         <a href="https://discord.gg/Fy4WmSThRZ">
                             <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/924312493537509407/721de16575a9048c9c6980613ebb99f0.webp" />
+                            <img className="rounded-full h-32 w-32" src="/img/logo.jpg" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">The Developers</h1>        
                                 <div className="flex items-center mt-1">
