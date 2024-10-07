90a6e03e4c8a25ae9019816a981f6650c0382c84
pogoosstudio <dioverrdm@gmail.com>
2024-10-07 13:20:35 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,20 +10,20 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Astra Bot
+          Pogo OS
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
-	  </p>
+       Con casi todas las funciones que pueda imaginar, además de personal dedicado y un equipo de soporte activo, Pogo OS es el complemento perfecto para cualquier servidor Discord.
+	</p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://bit.ly/3PHDjyC"}>
+            <Link href={"https://discord.com/oauth2/authorize?client_id=814494780251045928"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Astra
+          Invítame
               </a>
             </Link>
   <div className="py-10"></div>
